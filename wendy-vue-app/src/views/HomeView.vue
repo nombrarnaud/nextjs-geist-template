cf47ad295db4b35c1da6024bc6456e65becf7446
nombrarnaud <133082129+nombrarnaud@users.noreply.github.com>
2025-07-08 23:58:02 -0400

    Add wendy-vue-app/src/views/HomeView.vue 1752033482316

---
+++ wendy-vue-app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="min-h-screen">
+    <Hero />
+    <Services />
+    <Partners />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Hero from '@/components/Hero.vue'
+import Services from '@/components/Services.vue'
+import Partners from '@/components/Partners.vue'
+</script>
