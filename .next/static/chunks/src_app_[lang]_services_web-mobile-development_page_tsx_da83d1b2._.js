47140bfd68a8a5bbe9004a909e83647b3cab7a0c
nombrarnaud <133082129+nombrarnaud@users.noreply.github.com>
2025-07-08 23:58:27 -0400

    Add .next/static/chunks/src_app_[lang]_services_web-mobile-development_page_tsx_da83d1b2._.js 1752033507415

---
+++ .next/static/chunks/src_app_[lang]_services_web-mobile-development_page_tsx_da83d1b2._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_83d8c289._.js"
+],
+    source: "dynamic"
+});
