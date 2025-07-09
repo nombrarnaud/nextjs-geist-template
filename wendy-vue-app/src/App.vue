6697dedcb6af2212c6dc0d2ceb87d7fac8467e7b
nombrarnaud <133082129+nombrarnaud@users.noreply.github.com>
2025-07-08 23:58:12 -0400

    Add wendy-vue-app/src/App.vue 1752033492347

---
+++ wendy-vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="min-h-screen">
+    <Navbar />
+    <RouterView />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  max-width: none;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
