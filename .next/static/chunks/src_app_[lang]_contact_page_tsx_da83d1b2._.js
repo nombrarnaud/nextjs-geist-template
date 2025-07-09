8757c0d9180655e18148df9095df5a2a4a8081b0
nombrarnaud <133082129+nombrarnaud@users.noreply.github.com>
2025-07-08 23:58:24 -0400

    Add .next/static/chunks/src_app_[lang]_contact_page_tsx_da83d1b2._.js 1752033504090

---
+++ .next/static/chunks/src_app_[lang]_contact_page_tsx_da83d1b2._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_be5176ac._.js"
+],
+    source: "dynamic"
+});
